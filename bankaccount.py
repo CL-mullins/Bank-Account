62638bc4975747a6c844a715862651cdb483af0d
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-29 13:17:48 -0700

    Initialized class attributes

--- bankaccount.py
+++ bankaccount.py
@@ -1,7 +1,10 @@
 class BankAccount:
-    self.full_name
-    self.account_number
-    self.routing_number
-    self.balance
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
 
-    def deposit(amount)
+    def deposit(amount):
+        return amount + balance
+        print(f'Amount Deposited: ${amount}') 

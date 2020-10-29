b592991a0470dd6543801c232ca6e75f4bb1b208
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-29 13:35:51 -0700

    testing methods
    currently there is an issue where my deposit method won't run due to an unboundlocal error. Seeking help

--- bankaccount.py
+++ bankaccount.py
@@ -6,7 +6,6 @@ class BankAccount:
         self.balance = balance
 
     def deposit(amount):
-        return amount + balance
         balance = amount + balance
         print(f'Amount Deposited: ${amount}') 
 
@@ -15,14 +14,18 @@ class BankAccount:
         balance = balance - amount
         print(f'Amount Deposited: ${amount}')
 
-    def get_balance()
+    def get_balance():
         print(f'Hello, {full_name}')
         printf(f'Your balance is {balance}')
 
-    def add_interest()
+    def add_interest():
         interest = balance *  0.00083
         balance = balance + interest
         return balance
 
 
+
+SamJohnson = BankAccount('Sam Johnson', 82712391, 38914091, 500.00)
 print("Hello, I'm Banksy, your personal banking assistant!")
+
+SamJohnson.deposit()

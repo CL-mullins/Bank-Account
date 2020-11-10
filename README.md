2ba5d06cf4e0a716f8947ccbc76b70f6197a517e
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-11-10 12:49:51 -0800

    added comments & more info
    Added more detailed information to comments and readme.md

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 Creating a Bank Account for my CS 1.1 Assignment
+
+**IMPORTANT**
+This program creates a bank account class to be used to create 4 different bank accounts with different account numbers, the same routing number and various account balances. There are 5 Bank Account methods specified below:
+get_balance(): Returns the user's balance
+deposit(): Deposits money into the user's bank account
+withdraw(): Withdraws money from the user's bank account
+    If there is an insufficient amount of funds, the bank account will be overdrawn and a fee will be applied
+print_reciept(): Prints the current reciept of the user's bank account that specifies name, routing number, account balance.
+
+
+
+ASSIGNMENT REQUIREMENTS
+This bank account should all a user to do the following:
+
+Your Python program should be created in one file called BankAccount.py.
+
+Define a BankAccount class.
+
+A bank account should have the following attributes:
+
+full_name - the full name of the bank account account owner.
+account_number - randomly generated 8 digit number, unique per account.
+routing_number - 9 digit number, this number is the same for all accounts.
+balance - the balance of money in the account, should start at 0.
+Then define the following methods for the BankAccount class:
+
+The deposit method will take one parameter amount and will add amount to the balance. Also, it will print the message: “Amount Deposited: $X.XX”
+
+The withdraw method will take one parameter amount and will subtract amount from the balance. Also, it will print a message, like “Amount Withdrawn: $X.XX”. If the user tries to withdraw an amount that is greater than the current balance, print ”Insufficient funds.” and charge them with an overdraft fee of $10.
+
+The get_balance method will print a user-friendly message with the account balance and then also return the current balance of the account.
+
+The add_interest method adds interest to the users balance. The annual interest rate is 1% (i.e. 0.083% per month). Thus, the monthly interest is calculated by the following equation: interest = balance *  0.00083 .
+
+The print_receipt method prints a receipt with the account name, account number, and balance like this:
+
+Joi Anderson
+Account No.: ****5678
+Routing No.: 98765432
+Balance: $100.00 
+📋 Click to Copy✓
+Outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
+
+Your examples should show you using the different methods above to demonstrate them working.
--- bankaccount.py
+++ bankaccount.py
@@ -1,35 +1,46 @@
 class BankAccount:
+    #Creates a bank account class with 5 methods for various functions defined below
     def __init__(self, full_name, account_number, routing_number, balance):
+        #Initializes the bank account class with the user's name, randomized account number, and balance.
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+    
 
     def deposit(self,amount):
+        #Function that deposits money into the specified user's bank account. Increasing their balance by a specified amount.
+        #Function prints bank account balance in dollar measure
         self.balance = amount + self.balance
         print(f'Amount Deposited: ${amount}') 
 
     def widthdraw(self,amount):
+        #Function that withdraws money from the user's bank account. If the withdrawal amount is over the user's current balance, the amount is withdrawn
+        #however, there will be an insufficient funds statement and an overdraft fee of $10 applied.
         if amount > self.balance:
             self.balance = self.balance - amount
             print("Insufficient Funds")
             self.balance = self.balance - 10
             print(f'Amount Withdrawn ${amount}')
-            #Overdraft fee
+            #Overdraft fee applied here
 
         else:
             self.balance = self.balance - amount
             print(f'Amount Withdrawn: ${amount}')
+            #Situation where the balance exceeds withdrawn amount
 
     def get_balance(self):
+        #Prints a welcome message, then prints the balance of the user's account rounded to a dollar value
         print(f'Hello, {self.full_name}')
         print(f'Your balance is ${round(self.balance,2)}')
 
     def add_interest(self):
+        #Adds a fixed amount of interest to the users account
         interest = self.balance *  0.00083
         self.balance = self.balance + interest
 
     def print_receipt(self):
+        #Reports various attributes of the user's account including account number, name, balance, and routing number
         print('              ')
         print(self.full_name)
         censor = "****"
@@ -38,23 +49,34 @@ class BankAccount:
         print(f'Balance: ${round(self.balance,2)}')
 
 
-
+#Initialization of various different accounts
 SamJohnson = BankAccount('Sam Johnson', '31232391', 38914091, 500.00)
 RickJameson = BankAccount('Rick Jameson', '41929381',38914091,250)
 JaneDoe = BankAccount('Jane Doe', '12391238',38914091, 1000)
 JohnDoe = BankAccount('John Doe','31906220', 38914091, 25)
 
+#Chatbot functionality
 print('             ')
 print("Hello, I'm Banksy, your personal banking assistant!")
 print('     ')
 
+#Example of the get_balance function
 SamJohnson.get_balance()
+
+#Example of the deposit function
 SamJohnson.deposit(6.53)
+
+#Example of the withdraw function
 SamJohnson.widthdraw(600)
+
+#Example of adding interest
 SamJohnson.add_interest()
+
+#Prints balance once again.
 SamJohnson.get_balance()
 
 print('    ')
+#Below are the reciepts of the four bank accounts that I initialized
 SamJohnson.print_receipt()
 
 RickJameson.print_receipt()
